2ea6535da4da80f344605b460a234818d9a139db
Peter <trambroid@gmail.com>
2014-11-07 14:21:23 +0200

    Update settings.py

--- cattomato/settings.py
+++ cattomato/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3j+pz#(fngr*fr5scdc$x*9pmp0fi!ek&@ttw6cc06!g#kkb&d'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
